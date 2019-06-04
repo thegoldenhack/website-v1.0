58ce4c7ddd608839d2778091c54b07792711a3a2
Dunja <d2tomic@edu.uwaterloo.ca>
2019-06-04 00:42:04 -0400

    Moved scss -> css

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -15,7 +15,7 @@ module.exports = {
   node: {
     fs: 'empty',
   },
-  entry: ['./src/js/app.js', './src/style/main.scss'],
+  entry: ['./src/js/app.js', './src/style/main.css'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js',
